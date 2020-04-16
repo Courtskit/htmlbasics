ad5ed1eb13511bfcfc81241c8b908f5ec2034f90
Courtskit <Courtskit@gmail.com>
2020-04-15 21:50:13 -0700

    updated some js

---
+++ app.js
@@ -0,0 +1,12 @@
+// `use strict`;
+
+var avenger = prompt("Who is your favorite Marvel character?");
+
+if (avenger == "iron man") {
+  alert("You are correct.");
+} else {
+  alert("Wrong");
+}
+
+var today = new Date();
+document.write(today);
